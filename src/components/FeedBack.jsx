cde6453cede6d08d85bd23a4d1ebaac807221e39
Devrajsinh <103804567+Devrajsinh03@users.noreply.github.com>
2022-10-31 21:15:11 +0530

    Update FeedBack.jsx

--- src/components/FeedBack.jsx
+++ src/components/FeedBack.jsx
@@ -22,7 +22,7 @@ export default function FeedBack() {
           <div class="review">
             <div class="fc">
               <img
-                src="https://bslthemes.com/arter/wp-content/uploads/2020/09/testimonial-2-140x140.jpg"
+                src="https://raw.githubusercontent.com/Devrajsinh03/KPD-portfolio-up/new_pics/src/assets/rupen.jpg"
                 alt=""
               />
             </div>
@@ -81,7 +81,7 @@ export default function FeedBack() {
               Great work I would say, it was an amazing experience working with for making a blogging website for my youtube channel. 
             </center>
             <div class="m-title">Nency</div>
-            <div class="brow">Hello Guyzzz</div>
+            <div class="brow">Youtuber</div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -103,14 +103,14 @@ export default function FeedBack() {
               He made me an great E-commerce website for my battery business. Being a Baniya I would say it was worth spending money here.
             </center>
             <div class="m-title">Jash Sheth</div>
-            <div class="brow">Baniya/CMNG</div>
+            <div class="brow">Pyhton programmer</div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div class="review">
             <div class="fc">
               <img
-                src="https://bslthemes.com/arter/wp-content/uploads/2020/09/testimonial-2-140x140.jpg"
+                src="https://raw.githubusercontent.com/Devrajsinh03/KPD-portfolio-up/new_pics/src/assets/hemant.jpg"
                 alt=""
               />
             </div>
@@ -125,14 +125,14 @@ export default function FeedBack() {
               Thanks to Priyansh, now I have an amazing website for my NGO which helps boys to find their Happiness. On it they can do what they love.
             </center>
             <div class="m-title">Hemant</div>
-            <div class="brow">Sugar Daddy</div>
+            <div class="brow">Client</div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div class="review">
             <div class="fc">
               <img
-                src="https://raw.githubusercontent.com/Devrajsinh03/KPD-portfolio-up/main/src/assets/dev.jpg"
+                src="https://raw.githubusercontent.com/Devrajsinh03/KPD-portfolio-up/new_pics/src/assets/dev.jpg"
                 alt=""
               />
             </div>
@@ -144,10 +144,33 @@ export default function FeedBack() {
               <i class="fa fa-star" aria-hidden="true"></i>
             </div>
             <center class="s-title">
-              I work for a freelancing agency, our client wanted a website according to the UI the provided. And Priyansh helped us building that website.
+              It is an amazing experience working with Priyansh on web development projects. I feel amazed looking at his skills and creativity.
             </center>
-            <div class="m-title">Dev</div>
-            <div class="brow">playchokro</div>
+            <div class="m-title">Dev Joshi</div>
+            <div class="brow">Client</div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class="review">
+            <div class="fc">
+              <img
+                src="https://raw.githubusercontent.com/Devrajsinh03/KPD-portfolio-up/new_pics/src/assets/uttam.jpg"
+                alt=""
+              />
+            </div>
+            <div class="stars">
+              <i class="fa fa-star" aria-hidden="true"></i>
+              <i class="fa fa-star" aria-hidden="true"></i>
+              <i class="fa fa-star" aria-hidden="true"></i>
+              <i class="fa fa-star" aria-hidden="true"></i>
+              <i class="fa fa-star" aria-hidden="true"></i>
+            </div>
+            <center class="s-title">
+            Priyansh is a helpful, motivated, professional front-end developer.
+I was new to front-end development when working with him, but he was very patient with me.He is a superb front-end developer who always delivers clean and quick code.His strongest point is front-end/interface development and he's nicknamed the javascript king.
+            </center>
+            <div class="m-title">Uttam Buva</div>
+            <div class="brow">UI/UX designer</div>
           </div>
         </SwiperSlide>
       </Swiper>
